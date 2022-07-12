87abd3cbb03180f5fe1d0cab65650213e8896742
“terry19970422” <terry19970422@outlook.com>
2022-07-12 09:59:29 +0800

    單向資料流

---
+++ src/ex-0712-1/Child.js
@@ -0,0 +1,6 @@
+function Child(props) {
+  console.log(props)
+  return <h1>{props.text}</h1>
+}
+
+export default Child
---
+++ src/ex-0712-1/Parent.js
@@ -0,0 +1,11 @@
+import Child from './Child'
+
+function Parent() {
+  return (
+    <>
+      <Child text="hello" />
+    </>
+  )
+}
+
+export default Parent
