ae067c9030b5003c5d1c4dc395f4a60443559a11
“terry19970422” <terry19970422@outlook.com>
2022-07-12 10:23:38 +0800

    父層增加物件與函式，子層解構

--- src/ex-0712-1/Child.js
+++ src/ex-0712-1/Child.js
@@ -1,6 +1,13 @@
 function Child(props) {
+  const { text, abc, isBook, foo } = props
   console.log(props)
-  return <h1>{props.text}</h1>
+  return (
+    <>
+      <h1>{text}</h1>
+      <p>{abc}</p>
+      <button onClick={foo}>Click Me</button>
+    </>
+  )
 }
 
 export default Child
--- src/ex-0712-1/Parent.js
+++ src/ex-0712-1/Parent.js
@@ -3,7 +3,14 @@ import Child from './Child'
 function Parent() {
   return (
     <>
-      <Child text="hello" />
+      <Child
+        text="hello"
+        a={123}
+        isBook={true}
+        foo={() => {
+          alert('hello')
+        }}
+      />
     </>
   )
 }
