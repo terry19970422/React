2c6aca816fb66c380df86140a28f6128df1ad4fa
“terry19970422” <terry19970422@outlook.com>
2022-07-13 16:51:34 +0800

    input&textarea&radio表單元素

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import OrderPage from './ex-0713-1/OrderPage'
+import Controlled from './ex-0713-2/Controlled'
 
 function App() {
   return (
     <>
-      <OrderPage />
+      <Controlled />
     </>
   )
 }
---
+++ src/ex-0713-2/Controlled.js
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+
+function Controlled() {
+  const [inputText, setInputText] = useState('')
+  const [textArea, setTextArea] = useState('')
+
+  //radio group
+  const [gender, setGender] = useState('')
+  const genderOptions = ['男', '女', '不提供']
+
+  return (
+    <>
+      <section id="inputText">
+        <h1>文字輸入框(input-text)</h1>
+        <input
+          type="text"
+          value={inputText}
+          onChange={(e) => {
+            setInputText(e.target.value)
+          }}
+        />
+      </section>
+      <section id="textArea">
+        <h1>文字輸入區域(textarea)</h1>
+        <textarea
+          value={textArea}
+          onChange={(e) => {
+            setTextArea(e.target.value)
+          }}
+        />
+      </section>
+      <section id="radio-group">
+        <h1>選項按鈕-群組(radio-group)</h1>
+        {genderOptions.map((v, i) => {
+          return (
+            <div key={i}>
+              <input
+                type="radio"
+                checked={gender === v}
+                value={v}
+                onChange={(e) => {
+                  setGender(e.target.value)
+                }}
+              />
+              <label>{v}</label>
+            </div>
+          )
+        })}
+      </section>
+    </>
+  )
+}
+
+export default Controlled
